5a13ef255baf5d5e94bd35ddeb12427f3d99609a
sreeshankarm <sreeshankar.m@duk.ac.in>
2026-02-14 20:28:30 +0530

    added files

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,11 +1,32 @@
+// export interface TokenResponse {
+//   access_token: string;
+//   // refresh_token: string;
+//   environment: string;
+// }
+
+// export const token = {
+//   getAccess: (): string | null =>
+//     localStorage.getItem("access_token"),
+
+//   set: (data: TokenResponse) => {
+//     localStorage.setItem("access_token", data.access_token);
+//     // localStorage.setItem("refresh_token", data.refresh_token);
+//     localStorage.setItem("environment", data.environment);
+//   },
+
+//   clear: () => localStorage.clear(),
+// };
+
+
+
 export interface TokenResponse {
   access_token: string;
-  // refresh_token: string;
-  environment: string;
+  environment: number;
 }
 
 export const token = {
-  getAccess: (): string | null => localStorage.getItem("access_token"),
+  getAccess: (): string | null =>
+    localStorage.getItem("access_token"),
 
   getEnvironment: (): number | null => {
     const env = localStorage.getItem("environment");
@@ -14,9 +35,11 @@ export const token = {
 
   set: (data: TokenResponse) => {
     localStorage.setItem("access_token", data.access_token);
-    // localStorage.setItem("refresh_token", data.refresh_token);
-    localStorage.setItem("environment", data.environment);
+    localStorage.setItem("environment", data.environment.toString());
   },
 
-  clear: () => localStorage.clear(),
+  clear: () => {
+    localStorage.removeItem("access_token");
+    localStorage.removeItem("environment");
+  },
 };
