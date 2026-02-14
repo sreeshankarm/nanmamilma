a327f5fc02cbcc8f6cde56b8d84cde6c718f405f
sreeshankarm <sreeshankar.m@duk.ac.in>
2026-02-14 18:00:31 +0530

    added files

--- src/Router/AppRouter.tsx
+++ src/Router/AppRouter.tsx
@@ -84,13 +84,47 @@
 
 
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import AuthRouter from "./AuthRouter";
+// import MainRouter from "./MainRouter";
+// import ProtectedRoute from "../Router/ProtectedRoute";
+
+// const AppRouter = () => (
+//   <BrowserRouter>
+//     <Routes>
+//       <Route path="/signin/*" element={<AuthRouter />} />
+//       <Route
+//         path="/*"
+//         element={
+//           <ProtectedRoute>
+//             <MainRouter />
+//           </ProtectedRoute>
+//         }
+//       />
+//     </Routes>
+//   </BrowserRouter>
+// );
+
+// export default AppRouter;
+
+
+
+
+
+
+
+
+
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
+
 import AuthRouter from "./AuthRouter";
 import MainRouter from "./MainRouter";
 import ProtectedRoute from "../Router/ProtectedRoute";
 
 const AppRouter = () => (
-  <BrowserRouter>
+  // <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/signin/*" element={<AuthRouter />} />
       <Route
@@ -102,7 +136,9 @@ const AppRouter = () => (
         }
       />
     </Routes>
-  </BrowserRouter>
+   {/* </BrowserRouter> */}
+   </HashRouter>
+
 );
 
 export default AppRouter;
