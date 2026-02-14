de73d0b1476f5ff7ba3bbd325558b4525382cc12
sreeshankarm <sreeshankar.m@duk.ac.in>
2026-02-14 17:26:47 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,49 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react({
+//       babel: {
+//         plugins: [['babel-plugin-react-compiler']],
+//       },
+//     }),
+//     tailwindcss(),
+//   ],
+// })
+
+
+
+
+
+
+
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler"]],
+      },
+    }),
+    tailwindcss(),
+  ],
+
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://nanmastagingapi.milma.in",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
+
